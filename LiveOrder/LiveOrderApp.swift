fae4ea5d8f04b7dd4e024e913500c6b746c69f22
Erik Flores <erikfloresq@icloud.com>
2022-11-14 19:24:18 -0500

    Initial Commit

---
+++ LiveOrder/LiveOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveOrderApp.swift
+//  LiveOrder
+//
+//  Created by Erik Flores on 14/11/22.
+//
+
+import SwiftUI
+
+@main
+struct LiveOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
